e160922d526ca7dda0170b53a03ebcbee8cf6fdd
Ansh Sharma <147230122+ansh-sx@users.noreply.github.com>
2025-01-05 22:38:13 +0530

    Update _document.tsx

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -6,6 +6,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="monetag" content="9dd22988c9ea6a2e288249357e753eb9" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: "(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',8747863,document.createElement('script'))",
+          }}
+        />
       </Head>
       <body>
         <Main />
